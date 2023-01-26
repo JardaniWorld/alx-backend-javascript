0e6591c5503190ffe508eb806328604a36926a37
John Agbo <Jonchukwuma04@gmail.com>
2023-01-26 12:39:12 -0800

    This script creates a class with name: ClasRoom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,14 @@
+/**
+ * This code represents a class 'ClassRoom'.
+ */
+
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
